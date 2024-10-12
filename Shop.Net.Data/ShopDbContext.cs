1fe26cb7e061c8e85990b6c4b168cc849894b0d7
Mahfuzur Rahman <mahfuzur.rhmn@brainstation-23.com>
2024-10-12 13:47:52 +0600

    country, city, state dbset

--- Shop.Net.Data/ShopDbContext.cs
+++ Shop.Net.Data/ShopDbContext.cs
@@ -1,8 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Shop.Net.Core.Domains.Directories;
 
 namespace Shop.Net.Data;
 
 public class ShopDbContext : DbContext
 {
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<StateProvince> StateProvinces { get; set; }
+    public DbSet<City> Cities { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Country>(entity =>
+        {
+            entity.Property(e => e.ThreeLetterCode)
+                .IsFixedLength();
+
+            entity.Property(e => e.TwoLetterCode)
+                .IsFixedLength();
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
 }

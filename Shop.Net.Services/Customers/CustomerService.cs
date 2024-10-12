c340ba37f606ccf6a08c6ce1f0a8db0c721cf65c
Mahfuzur Rahman <mahfuzur.rhmn@brainstation-23.com>
2024-10-12 19:54:11 +0600

    working on customer service

--- Shop.Net.Services/Customers/CustomerService.cs
+++ Shop.Net.Services/Customers/CustomerService.cs
@@ -1,1 +1,24 @@
+using System;
+using System.Threading.Tasks;
+using Shop.Net.Core.Domains.Customers;
+using Shop.Net.Data;
+
 namespace Shop.Net.Services.Customers;
+
+public class CustomerService
+{
+    protected readonly IRepository<Customer> customerRepository;
+
+    public CustomerService(IRepository<Customer> customerRepository)
+    {
+        this.customerRepository = customerRepository;
+    }
+
+    public async Task<Customer?> GetCustomerByIdAsync(int customerId)
+    {
+        if (customerId <= 0)
+            throw new ArgumentException(nameof(customerId));
+
+        return await customerRepository.GetOneByIdAsync(customerId);
+    }
+}

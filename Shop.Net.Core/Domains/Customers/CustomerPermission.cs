c53ace79bac2beee3a099e868863315bd69c4d7a
Mahfuzur Rahman <mahfuzur.rhmn@brainstation-23.com>
2024-10-12 17:27:06 +0600

    permission maxlength

--- Shop.Net.Core/Domains/Customers/CustomerPermission.cs
+++ Shop.Net.Core/Domains/Customers/CustomerPermission.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Shop.Net.Core.Domains.Customers;
 
 public class CustomerPermission : BaseEntity
 {
+    [MaxLength(64)]
     public string Permission { get; set; }
 
     public int CustomerId { get; set; }
--- Shop.Net.Core/Domains/Customers/RolePermission.cs
+++ Shop.Net.Core/Domains/Customers/RolePermission.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Shop.Net.Core.Domains.Customers;
 
 public class RolePermission : BaseEntity
 {
+    [MaxLength(64)]
     public string Permission { get; set; }
 
     public int RoleId { get; set; }

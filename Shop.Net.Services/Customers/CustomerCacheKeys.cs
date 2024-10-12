b9be5a02494f4838350a7c0c8c3452061bbe875e
Mahfuzur Rahman <mahfuzur.rhmn@brainstation-23.com>
2024-10-12 22:59:59 +0600

    password cachekey

--- Shop.Net.Services/Customers/CustomerCacheKeys.cs
+++ Shop.Net.Services/Customers/CustomerCacheKeys.cs
@@ -4,8 +4,11 @@ namespace Shop.Net.Services.Customers;
 
 public static class CustomerCacheKeys
 {
-    public const string PREFIX = "Shop.Net.Customers.GetCustomerById";
+    public const string PREFIX = "Shop.Net.Customers";
 
     public static CacheKey GetCustomerByIdKey => new CacheKey(
         "Shop.Net.Customers.GetCustomerById.{0}", PREFIX);
+
+    public static CacheKey GetPasswordByCustomerIdKey => new CacheKey(
+        "Shop.Net.Customers.GetPasswordByCustomerId.{0}", PREFIX);
 }

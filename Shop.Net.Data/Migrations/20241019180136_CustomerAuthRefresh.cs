337cb202e7f8970009ebb93842dd8977c2ca159b
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-10-20 00:01:42 +0600

    CustomerAuthRefresh

---
+++ Shop.Net.Data/Migrations/20241019180136_CustomerAuthRefresh.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Net.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CustomerAuthRefresh : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "RequireAuthRefresh",
+                table: "Customers",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RequireAuthRefresh",
+                table: "Customers");
+        }
+    }
+}

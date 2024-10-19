b323db0b91d4f2dac49cfacaf516d06a2ae36c37
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-10-19 17:57:18 +0600

    ThemeToDarkMode

---
+++ Shop.Net.Data/Migrations/20241019115710_ThemeToDarkMode.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Net.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThemeToDarkMode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Theme",
+                table: "Customers");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DarkModeEnabled",
+                table: "Customers",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DarkModeEnabled",
+                table: "Customers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Theme",
+                table: "Customers",
+                type: "varchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}

41e096e09d73a85661fcf8a7143d691ce2abcb1b
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-10-19 18:29:09 +0600

    dark mode toggle implementation complete

--- Shop.Net.Web.Admin/Program.cs
+++ Shop.Net.Web.Admin/Program.cs
@@ -3,36 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
-// var builder = WebApplication.CreateBuilder(args);
-
-// // Add services to the container.
-// builder.Services.ConfigureServices(builder.Configuration);
-
-// builder.Services.AddControllersWithViews();
-
-// var app = builder.Build();
-
-// // Configure the HTTP request pipeline.
-// if (!app.Environment.IsDevelopment())
-// {
-//     app.UseExceptionHandler("/Home/Error");
-//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//     app.UseHsts();
-// }
-
-// app.UseHttpsRedirection();
-// app.UseStaticFiles();
-
-// app.UseRouting();
-
-// app.ConfigureApplication();
-
-// app.MapControllerRoute(
-//     name: "default",
-//     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-// app.Run();
-
 namespace Shop.Net.Web.Admin;
 
 public class Program

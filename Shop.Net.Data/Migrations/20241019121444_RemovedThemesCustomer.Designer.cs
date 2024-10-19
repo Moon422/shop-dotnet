ac120bac9e21b1035a0f8b87e61c6bff0e5d16ec
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-10-19 18:14:51 +0600

    RemovedThemesCustomer

--- Shop.Net.Data/Migrations/20241019121444_RemovedThemesCustomer.Designer.cs
+++ Shop.Net.Data/Migrations/20241019121444_RemovedThemesCustomer.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Shop.Net.Data;
 
@@ -11,9 +12,11 @@ using Shop.Net.Data;
 namespace Shop.Net.Data.Migrations
 {
     [DbContext(typeof(ShopDbContext))]
-    partial class ShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241019121444_RemovedThemesCustomer")]
+    partial class RemovedThemesCustomer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Shop.Net.Data/Migrations/20241019121444_RemovedThemesCustomer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Net.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedThemesCustomer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DarkModeEnabled",
+                table: "Customers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "DarkModeEnabled",
+                table: "Customers",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}

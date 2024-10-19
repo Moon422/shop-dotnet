b96d2af315c6417e0e6ce9d0cf09939afc72d4ae
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-10-19 14:36:08 +0600

    CustomerTheme

---
+++ Shop.Net.Data/Migrations/20241019083603_CustomerTheme.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Net.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CustomerTheme : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Theme",
+                table: "Customers",
+                type: "varchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Theme",
+                table: "Customers");
+        }
+    }
+}

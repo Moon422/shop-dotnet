9b7b330739ca02544ca12107ba5b28787d7c2e7b
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-10-21 10:01:22 +0600

    working data tabels

--- Shop.Net.Web.Admin/Controllers/CustomerController.cs
+++ Shop.Net.Web.Admin/Controllers/CustomerController.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Shop.Net.Web.Admin.Factories;
 using Shop.Net.Web.Admin.Filters;
 using Shop.Net.Web.Admin.Models;
 using Shop.Net.Web.Admin.Models.Common;
+using Shop.Net.Web.Admin.Models.Customers;
 
 namespace Shop.Net.Web.Admin.Controllers;
 
@@ -25,20 +29,31 @@ public class CustomerController : BaseController
     public async Task<IActionResult> List()
     {
         var searchModel = await customerModelFactory.PrepareCustomerSearchModelAsync(new CustomerSearchModel());
-        var model = new TableModel()
-        {
-            Columns = ["ABc", "DEf", "GHi"]
-        };
 
-        return View("_Table", model);
+        return View(searchModel);
     }
 
     [HttpPost]
     [ActionName("List")]
     [PermissionAuthorize("customers:list:all, customers:list:read")]
-    public async Task<IActionResult> ListCustomer()
+    public async Task<IActionResult> ListCustomer(CustomerSearchModel searchModel)
     {
-        var searchModel = await customerModelFactory.PrepareCustomerSearchModelAsync(new CustomerSearchModel());
-        return View(searchModel);
+        var model = new List<CustomerModel>()
+        {
+            new CustomerModel()
+            {
+                Id = 3,
+                FirstName = "Mahfuzur",
+                LastName = "Rahman"
+            },
+            new CustomerModel()
+            {
+                Id = 4,
+                FirstName = "Fatima",
+                LastName = "Ajwah"
+            }
+        };
+
+        return Json(model);
     }
 }
